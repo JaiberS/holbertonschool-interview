6fadda6027e4a04363420175a4ea10e0dd645fe1
JaiberS <758@holbertonschool.com>
2020-12-09 10:28:56 -0500

    validation for key in range

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,9 +8,10 @@ def canUnlockAll(boxes):
         return True
     keys = [0]
     for j in keys:
-        for i in boxes[j]:
-            if i not in keys:
-                keys.append(i)
-            if len(keys) == len(boxes):
-                return True
+        if j < len(boxes):
+            for i in boxes[j]:
+                if i not in keys:
+                    keys.append(i)
+                if len(keys) == len(boxes):
+                    return True
     return False
